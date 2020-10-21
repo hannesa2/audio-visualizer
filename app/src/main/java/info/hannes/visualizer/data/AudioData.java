6c5d88caa3ef067429b3e161deb358d0bb5021a0
Hannes Achleitner <hannes.software@gmx.at>
2020-10-21 08:08:05 +0200

    Initial commit

---
+++ app/src/main/java/info/hannes/visualizer/data/AudioData.java
@@ -0,0 +1,9 @@
+package info.hannes.visualizer.data;
+
+public class AudioData {
+    public AudioData(byte[] bytes) {
+        this.bytes = bytes;
+    }
+
+    public byte[] bytes;
+}
---
+++ app/src/main/java/info/hannes/visualizer/data/FFTData.java
@@ -0,0 +1,9 @@
+package info.hannes.visualizer.data;
+
+public class FFTData {
+    public FFTData(byte[] bytes) {
+        this.bytes = bytes;
+    }
+
+    public byte[] bytes;
+}
